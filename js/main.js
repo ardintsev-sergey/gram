4fbe0c5ad21664d8e628bbe6088bf160e28fc45b
Sergey Ardintsev <archunter13@yandex.ru>
2024-03-17 22:15:39 +0300

    debouce added

--- js/main.js
+++ js/main.js
@@ -9,13 +9,14 @@ import { setFormSubmit } from './validate.js';
 import { hideModal } from './user-form.js';
 import { getData } from './api.js';
 import {  comparePopularPhoto, compareRandomPhoto, renderPictures, setDefaultSortClick, setPopularSortClick, setRandomSortClick } from './render-picture.js';
-
+import { debounce } from './debounce.js';
+const RERENDER_DELAY = 500;
 
 getData((data) => {
   renderPictures(data, 25);
-  setDefaultSortClick(() => renderPictures(data, 25));
-  setRandomSortClick(() => renderPictures(data, 10, compareRandomPhoto));
-  setPopularSortClick(() => renderPictures(data, 25, comparePopularPhoto));
+  setDefaultSortClick(debounce(() => renderPictures(data, 25), RERENDER_DELAY));
+  setRandomSortClick(debounce(() => renderPictures(data, 10, compareRandomPhoto), RERENDER_DELAY));
+  setPopularSortClick(debounce(() => renderPictures(data, 25, comparePopularPhoto), RERENDER_DELAY));
 }
   // (data) => sortBtn.addEventListener('click', ()=> {
   //   renderPopularPhoto(data);
